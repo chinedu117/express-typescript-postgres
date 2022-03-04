811a14761e2939e46bde1c0148d0e6a96a799fb6
chinedu117 <chinedudimonyeka@gmail.com>
2022-03-04 09:06:58 +0100

    Renames sequelize config.json to .js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
-{
+require("dotenv").config();
+
+console.log(process.env.DB_USER);
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
